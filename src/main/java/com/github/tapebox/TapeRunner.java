6db5aef4d05d3a24f0703039c96b4674fe5cb60f
zyw271828 <zyw_0618@outlook.com>
2021-01-13 10:53:24 +0800

    Implement TapeRunner

--- src/main/java/com/github/tapebox/TapeRunner.java
+++ src/main/java/com/github/tapebox/TapeRunner.java
@@ -1,8 +1,27 @@
 package com.github.tapebox;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TapeRunner {
     static String runTape(String filePath, String tx) throws Exception {
-        // TODO: unimplemented method
-        return "";
+        String result = null;
+
+        try {
+            Process p = Runtime.getRuntime().exec(new String[] { "tape", "--config=" + filePath, "--number=" + tx });
+
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((result = stdInput.readLine()) != null) {
+                stringBuilder.append(result);
+                stringBuilder.append('\n');
+            }
+            result = stringBuilder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
