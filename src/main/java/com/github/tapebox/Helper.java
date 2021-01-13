b2d0f568808e93f1e50dcfe08cd8e0ec16518ad7
zyw271828 <zyw_0618@outlook.com>
2021-01-13 10:28:20 +0800

    Add empty methods

---
+++ src/main/java/com/github/tapebox/Helper.java
@@ -0,0 +1,28 @@
+package com.github.tapebox;
+
+public class Helper {
+    static boolean isFileExist(String filePath) {
+        // TODO: unimplemented method
+        return true;
+    }
+
+    static boolean isNumber(String str) {
+        // TODO: unimplemented method
+        return true;
+    }
+
+    static String getTx(String str) throws Exception {
+        // TODO: unimplemented method
+        return "";
+    }
+
+    static String getDuration(String str) throws Exception {
+        // TODO: unimplemented method
+        return "";
+    }
+
+    static String getTps(String str) throws Exception {
+        // TODO: unimplemented method
+        return "";
+    }
+}
---
+++ src/main/java/com/github/tapebox/TapeRunner.java
@@ -0,0 +1,8 @@
+package com.github.tapebox;
+
+public class TapeRunner {
+    static String runTape(String filePath, String tx) throws Exception {
+        // TODO: unimplemented method
+        return "";
+    }
+}
